a6969a8aaa42911d456ec126b32d5ddbed7fe420
Sairacat <201769jzy@gmail.com>
2026-06-22 13:42:42 +0800

    Added a animation to list-unit and todos-card. 1.Added a fadeIn animaiton which will render the element from opacity 0/ translateY(10px) to opacity 1/ translateY(0).

--- src/displayController.js
+++ src/displayController.js
@@ -42,6 +42,7 @@ function IntializeListEvent() {
         e.preventDefault();
         addNewListToArray(listName.value)
         displayListName();
+        displayTodosCard();
         listDialog.close();
         form.reset();
     })
@@ -54,6 +55,7 @@ function displayListName() {
     for(const list of listArray) {
         const listUnit = document.createElement('div');
         listUnit.classList.add('list-unit');
+        listUnit.classList.add('unit-fadeIn');
 
         const listName = document.createElement('div');
         listName.textContent = list.name;
@@ -64,10 +66,36 @@ function displayListName() {
         deleteBtn.addEventListener('click', () => {
             deleteListFromArray(deleteBtn.id);
             displayListName();
+            displayTodosCard();
         })
 
         listUnit.append(listName, deleteBtn);
         listContainer.append(listUnit);
+    }
+}
+
+function displayTodosCard() {
+    const todosArea = document.querySelector('.todos-area');
+    todosArea.replaceChildren();
+
+    for(const list of listArray) {
+        const todosCard = document.createElement('div');
+        todosCard.classList.add('todos-card');
+        todosCard.classList.add('unit-fadeIn');
+
+        const cardTitle = document.createElement('div');
+        cardTitle.classList.add('card-title');
+
+        const listName = document.createElement('h2');
+        listName.textContent = list.name;
+
+        const addTodosBtn = document.createElement('div');
+        addTodosBtn.textContent = '+';
+        addTodosBtn.classList.add('addtodosbtn');
+
+        cardTitle.append(listName, addTodosBtn);
+        todosCard.append(cardTitle);
+        todosArea.append(todosCard);
     }
 }
 
